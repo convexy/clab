e78600ccd8442b46dc20e54ab7fc5923f073f9fe
convexy <shibuya.keigo@gmail.com>
2025-04-28 23:33:57 +0900

    first commit

---
+++ src/cobjects.ts
@@ -0,0 +1,55 @@
+import * as THREE from "three";
+import * as CANNON from "cannon";
+
+interface CObjectOptions {
+  mass?: number,
+  position?: { x: number, y: number, z: number },
+  velocity?: { x: number, y: number, z: number },
+  size?: number | { x: number, y: number, z: number }
+}
+
+export class CObject {
+  body: CANNON.Body;
+  mesh: THREE.Object3D;
+  constructor(body: CANNON.Body, mesh: THREE.Object3D) {
+    this.body = body;
+    this.mesh = mesh;
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+  }
+}
+
+export class CBall extends CObject {
+  constructor(options?: CObjectOptions) {
+    const size = typeof options?.size === "number" ? options?.size : 1;
+    const body = new CANNON.Body({
+      mass: options?.mass ?? 1,
+      shape: new CANNON.Sphere(size),
+      position: new CANNON.Vec3(options?.position?.x ?? 0, options?.position?.y ?? 0, options?.position?.z ?? 0),
+      velocity: new CANNON.Vec3(options?.velocity?.x ?? 0, options?.velocity?.y ?? 0, options?.velocity?.z ?? 0),
+    });
+    const geometry = new THREE.SphereGeometry(size);
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+    const mesh = new THREE.Mesh(geometry, material);
+    super(body, mesh);
+  }
+}
+
+export class CCube extends CObject {
+  constructor(options?: CObjectOptions) {
+    const size =
+      typeof options?.size === "number"
+        ? { x: options?.size ?? 1, y: options?.size ?? 1, z: options?.size ?? 1 }
+        : { x: options?.size?.x ?? 1, y: options?.size?.y ?? 1, z: options?.size?.z ?? 1 };
+    const body = new CANNON.Body({
+      mass: 1,
+      shape: new CANNON.Box(new CANNON.Vec3(size.x / 2, size.y / 2, size.z / 2)),
+      position: new CANNON.Vec3(options?.position?.x ?? 0, options?.position?.y ?? 0, options?.position?.z ?? 0),
+      velocity: new CANNON.Vec3(options?.velocity?.x ?? 0, options?.velocity?.y ?? 0, options?.velocity?.z ?? 0),
+    });
+    const geometry = new THREE.BoxGeometry(size.x, size.y, size.z);
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+    const mesh = new THREE.Mesh(geometry, material);
+    super(body, mesh);
+  }
+}
---
+++ src/cworldf.ts
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import * as CANNON from "cannon";
+
+import { CObject } from "./cobjects";
+
+export class CWorldF {
+  physics: CANNON.World;
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+  renderer: THREE.WebGLRenderer;
+  ambientLight: THREE.AmbientLight;
+  directionalLight: THREE.DirectionalLight;
+  cobjects: CObject[];
+  constructor() {
+    this.physics = new CANNON.World();
+    this.physics.gravity = new CANNON.Vec3(0, -9.82, 0);
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setClearColor(0xddeeff);
+    this.renderer.shadowMap.enabled = true;
+    document.body.appendChild(this.renderer.domElement);
+    this.camera.position.set(7, 5, 7);
+    this.camera.lookAt(0, 5, 0);
+
+    this.ambientLight = new THREE.AmbientLight(0xffffff);
+    this.scene.add(this.ambientLight);
+    this.directionalLight = new THREE.DirectionalLight(0xffffff);
+    this.directionalLight.position.set(10, 100, -10);
+    this.directionalLight.lookAt(0, 0, 0);
+    this.directionalLight.castShadow = true;
+    this.directionalLight.shadow.mapSize.width = 2048;
+    this.directionalLight.shadow.mapSize.height = 2048;
+    this.directionalLight.shadow.camera.left = -100;
+    this.directionalLight.shadow.camera.right = 100;
+    this.directionalLight.shadow.camera.top = 100;
+    this.directionalLight.shadow.camera.bottom = -100;
+    this.directionalLight.shadow.camera.near = 0.1;
+    this.directionalLight.shadow.camera.far = 200;
+    this.scene.add(this.directionalLight);
+
+    this.cobjects = [];
+
+    this.addHelper();
+    this.addGround();
+  }
+  addHelper() {
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
+    const gridHelper = new THREE.GridHelper(100, 100);
+    this.scene.add(gridHelper);
+  }
+  addGround() {
+    const groundBody = new CANNON.Body({
+      mass: 0,
+      shape: new CANNON.Plane(),
+      position: new CANNON.Vec3(0, 0, 0),
+    })
+    groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+    this.physics.addBody(groundBody);
+
+    const goundGeometry = new THREE.PlaneGeometry(100, 100);
+    const groundMaterial = new THREE.MeshPhongMaterial({ color: 0x888888 });
+    const groundMesh = new THREE.Mesh(goundGeometry, groundMaterial);
+    groundMesh.rotation.x = -Math.PI / 2;
+    groundMesh.receiveShadow = true;
+    this.scene.add(groundMesh);
+  }
+
+  addCObject(cobject: CObject) {
+    this.physics.addBody(cobject.body);
+    this.scene.add(cobject.mesh);
+    this.cobjects.push(cobject);
+  }
+
+  updateAndRender(deltaTime: number) {
+    this.physics.step(deltaTime);
+    this.cobjects.forEach(cobject => {
+      cobject.mesh.position.copy(cobject.body.position);
+      cobject.mesh.quaternion.copy(cobject.body.quaternion);
+    });
+    this.renderer.render(this.scene, this.camera);
+  }
+  setAnimationLoop(callback: XRFrameRequestCallback) {
+    this.renderer.setAnimationLoop(callback);
+  }
+}
